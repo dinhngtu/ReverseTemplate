a5a7f5e9553d3fcf8bc7301582a7b5ba605b79aa
Tu Dinh <dinhngtu@users.noreply.github.com>
2020-11-13 08:47:41 +0100

    cached template line: don't use capture name counter

--- Engine/CachedTemplateLine.cs
+++ Engine/CachedTemplateLine.cs
@@ -8,8 +8,6 @@ using System.Threading.Tasks;
 
 namespace ReverseTemplate.Engine {
     public class CachedTemplateLine {
-        static int uniqueCounter = 0;
-
         public TemplateLine TemplateLine { get; }
         public IReadOnlyList<LineSection> Sections { get; }
         public IReadOnlyList<string> CaptureNames { get; }
@@ -30,15 +28,15 @@ namespace ReverseTemplate.Engine {
             Sections = tl.Sections.Select(x => {
                 if (x is CaptureSection cs && cs.VarName == null) {
                     if (cs.Flags.HasFlag(CaptureFlags.SkipDataLineIfNotFound) || cs.Flags.HasFlag(CaptureFlags.SkipTemplateLineIfNotFound)) {
-                        return new CaptureSection(cs.Pattern, $"__ctl{uniqueCounter++}", cs.Flags);
+                        return new CaptureSection(cs.Pattern, $"__ctl", cs.Flags);
                     }
                 }
                 return x;
             }).ToList();
-            CaptureNames = Sections.OfType<CaptureSection>().Where(x => x.VarName != null).Select(x => x.VarName!).ToList();
+            CaptureNames = Sections.OfType<CaptureSection>().Where(cs => cs.VarName != null && cs.VarName != "__ctl").Select(x => x.VarName!).ToList();
             RegexString = ToRegex(Sections);
             RegexObject = new Regex(RegexString);
-            AllCaptureGroups = RegexObject.GetGroupNames().Where(x => !Regex.IsMatch(x, "^[0-9]*$")).ToList();
+            AllCaptureGroups = RegexObject.GetGroupNames().Where(g => g != "__ctl" && !Regex.IsMatch(g, "^[0-9]*$")).ToList();
         }
 
         public CachedTemplateLine(IEnumerable<LineSection> sections) : this(new TemplateLine(sections)) {
